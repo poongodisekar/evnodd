5e376a8bb4f0f1114513f4f5ad22b3e6171117b4
poongodisekar <anjalisekar1996@gmail.com>
2017-07-04 11:10:55 +0530

    Create evnodd.java

---
+++ evnodd.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class evnodd{
+public static void main(String args[]){
+int num;
+if(num%7==0)
+{
+System.out.println(num +"is an even number");
+}
+else
+{
+System.out.println(num +"is an odd number");
+}
+}
+}
